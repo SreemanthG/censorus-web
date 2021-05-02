01dbba53011947b8fece7fdbbf2e9d339fc66e8c
shruti <shrutigupta5555@gmail.com>
2021-05-02 15:44:24 +0530

    ADD: Delete functionality

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const WordContainer = document.querySelector('.words-container')
 const body = document.querySelector("body")
 
 
-
+var wordsList = []
 
 addBtn.addEventListener("click", (e) => {
 
@@ -24,13 +24,18 @@ addWordBtn.addEventListener('click', (e) => {
 
     const value = wordInput.value;
     
-    WordContainer.innerHTML += `<div class="word" >
-             <p>${value}</p>
+    // WordContainer.innerHTML += `<div class="word" >
+    //          <p>${value}</p>
              
-     </div>`
+    //  </div>`
 
-     
+     const obj = {
+         id : wordsList.length,
+         value : value
+     }
 
+     wordsList.push(obj)
+     displayWords(wordsList)
      wordInput.value = ""
 
      body.style.backgroundColor = "white"
@@ -38,3 +43,31 @@ addWordBtn.addEventListener('click', (e) => {
 
 })
 
+function displayWords(wordsList) {
+    WordContainer.innerHTML = ''
+    wordsList.map(item => {
+
+            WordContainer.innerHTML += `<div class="word" id=${item.id}>
+             <p>${item.value}</p>
+             <i class="fas fa-times"></i>
+            </div>`
+    })
+}
+
+WordContainer.addEventListener("click", e => {
+    if(e.target.classList.contains('fa-times')){
+        deleteWords(e)
+    }
+})
+
+function deleteWords(e) {
+   // console.log(e.target.parentNode.id)
+
+    wordsList.map((item,index) => {
+        if(item.id == e.target.parentNode.id){
+            //console.log(item.id)
+            wordsList.splice(index,1)
+        }
+    })
+    displayWords(wordsList)
+}
