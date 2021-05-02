32c5a6602a2f5f70295d4267eb38b936f6bd1631
shruti <shrutigupta5555@gmail.com>
2021-05-02 15:13:24 +0530

    ADD: Dashboard

---
+++ main.js
@@ -0,0 +1,40 @@
+const addBtn = document.querySelector('.fa-plus-circle')
+const popupDiv = document.querySelector('.popup-form')
+const wordInput = document.querySelector('.word-input')
+const addWordBtn = document.querySelector('.add-word')
+const WordContainer = document.querySelector('.words-container')
+const body = document.querySelector("body")
+
+
+
+
+addBtn.addEventListener("click", (e) => {
+
+  
+      
+  
+   popupDiv.classList.remove('hide')
+
+   body.style.backgroundColor = "#7389D5"
+  
+});
+
+addWordBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const value = wordInput.value;
+    
+    WordContainer.innerHTML += `<div class="word" >
+             <p>${value}</p>
+             
+     </div>`
+
+     
+
+     wordInput.value = ""
+
+     body.style.backgroundColor = "white"
+     popupDiv.classList.add('hide')
+
+})
+
