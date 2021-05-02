58b063381ae5484f23761ef04860e9278229a841
Saket Sarin <sarinsaket@gmail.com>
2021-05-02 15:32:52 +0530

    frontend

--- main.js
+++ main.js
@@ -1,40 +1,28 @@
-const addBtn = document.querySelector('.fa-plus-circle')
-const popupDiv = document.querySelector('.popup-form')
-const wordInput = document.querySelector('.word-input')
-const addWordBtn = document.querySelector('.add-word')
-const WordContainer = document.querySelector('.words-container')
-const body = document.querySelector("body")
-
-
-
+const addBtn = document.querySelector(".fa-plus-circle");
+const popupDiv = document.querySelector(".popup-form");
+const wordInput = document.querySelector(".word-input");
+const addWordBtn = document.querySelector(".add-word");
+const WordContainer = document.querySelector(".words-container");
+const body = document.querySelector("body");
 
 addBtn.addEventListener("click", (e) => {
+  popupDiv.classList.remove("hide");
 
-  
-      
-  
-   popupDiv.classList.remove('hide')
-
-   body.style.backgroundColor = "#7389D5"
-  
+  body.style.backgroundColor = "#7389D5";
 });
 
-addWordBtn.addEventListener('click', (e) => {
-    e.preventDefault()
+addWordBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    const value = wordInput.value;
-    
-    WordContainer.innerHTML += `<div class="word" >
+  const value = wordInput.value;
+
+  WordContainer.innerHTML += `<div class="word" >
              <p>${value}</p>
              
-     </div>`
+     </div>`;
 
-     
+  wordInput.value = "";
 
-     wordInput.value = ""
-
-     body.style.backgroundColor = "white"
-     popupDiv.classList.add('hide')
-
-})
-
+  body.style.backgroundColor = "white";
+  popupDiv.classList.add("hide");
+});
